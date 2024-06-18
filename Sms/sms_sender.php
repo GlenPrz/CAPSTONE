2d8f1f4f8919575c2e410bfc7d722a40bd1452ad
GlenPrz <168808657+GlenPrz@users.noreply.github.com>
2024-06-18 19:32:54 +0800

    Create sms_sender.php

---
+++ Sms/sms_sender.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="Sms_sending.php" method="post">
+    <label for="txt_msg">Enter your message:</label>
+    <input type="text" id="txt_msg" name="txt_msg">
+    <br>
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
+</form>
+
+</body>
+</html>
